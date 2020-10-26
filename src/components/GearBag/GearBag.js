3581c69569519a2cd2e329610404f65ad11eab64
Mason McElvain <masonmcelvain@users.noreply.github.com>
2020-10-26 09:51:06 -0700

    GearBag style updates

--- src/components/GearBag/GearBag.js
+++ src/components/GearBag/GearBag.js
@@ -1,16 +1,18 @@
 import React, { useState } from 'react';
 import './GearBag.css';
 
-import MyItemCard from '../MyItemCard/MyItemCard';
-
 export default function GearBag({ children }) {
 
   return (
     <div className="GearBag">
-      <div className="GearBag-card-container">
-        {children}
-        <div className="empty-card"></div>
-      </div>
+      {
+        children.length > 0 ?
+        <div className="GearBag-card-container">
+          {children}
+        </div>
+        :
+        <p className="GearBag-p">Add your first device!</p>
+      }
       <div className="GearBag-vertical-bar"></div>
     </div>
   );
